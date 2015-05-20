d3a809c2a668027403459ab6a862dde717f7ad5c
Michael <drmikeh@gmail.com>
2015-05-20 14:39:34 -0400

    Converted quotes.

--- ga-git-extras.rb
+++ ga-git-extras.rb
@@ -1,15 +1,15 @@
 class GaGitExtras < Formula
-  homepage "https://github.com/drmikeh/ga-git-extras"
-  url "https://github.com/drmikeh/ga-git-extras/archive/0.1.0.tar.gz"
-  sha256 "e225978ebc06acf7a5a8cd6ce41054c984834edfff5142b384eb50f9fee71bb4"
+  homepage 'https://github.com/drmikeh/ga-git-extras'
+  url 'https://github.com/drmikeh/ga-git-extras/archive/0.1.0.tar.gz'
+  sha256 'e225978ebc06acf7a5a8cd6ce41054c984834edfff5142b384eb50f9fee71bb4'
 
-  head "https://github.com/drmikeh/ga-git-extras.git", :branch => "master"
+  head 'https://github.com/drmikeh/ga-git-extras.git', :branch => 'master'
 
   # Don't take +x off these files
-  skip_clean "bin"
+  skip_clean 'bin'
 
   def install
-    inreplace "Makefile", %r{|\$\(DESTDIR\)(?=/etc/bash_completion\.d)|, "$(DESTDIR)$(PREFIX)"}
+    inreplace 'Makefile', %r|\$\(DESTDIR\)(?=/etc/bash_completion\.d)|, '$(DESTDIR)$(PREFIX)'
     system "make", "PREFIX=#{prefix}", "install"
   end
 end
