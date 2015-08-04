018c4d8e19111068480c509d7deef5434849098e
Mike Hopper <drmikeh@gmail.com>
2015-08-04 00:27:00 -0400

    Added sha1 for ga-wdi-extras.

--- ga-wdi-extras.rb
+++ ga-wdi-extras.rb
@@ -3,7 +3,7 @@ class GaWdiExtras < Formula
   homepage "https://github.com/drmikeh/ga-wdi-extras"
   url "https://github.com/drmikeh/ga-wdi-extras/archive/0.1.1.tar.gz"
   version "0.1.1"
-  sha256 ""
+  sha256 "d41a9351f4404cc8727108920040e2a1b2aab0957bda1970e373c12307790d45"
   head 'https://github.com/drmikeh/ga-git-extras.git', branch: 'master'
 
   # Don't take +x off these files
