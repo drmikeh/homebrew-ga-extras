aa6ea79b8ae37b7b1845c9fe043be144b44e49e5
Mike Hopper <drmikeh@gmail.com>
2015-08-04 00:45:57 -0400

    Bumped version for ga-wdi-extras.

--- ga-wdi-extras.rb
+++ ga-wdi-extras.rb
@@ -1,9 +1,9 @@
 class GaWdiExtras < Formula
   desc "A set of scripts in a brew keg for use with General Assembly's WDI class."
   homepage "https://github.com/drmikeh/ga-wdi-extras"
-  url "https://github.com/drmikeh/ga-wdi-extras/archive/0.1.2.tar.gz"
-  version "0.1.2"
-  sha256 "da93ae942cf1491ba6f1f3c8fbcc49acf0c98749cf4bfcfa18675e60a2f20bb7"
+  url "https://github.com/drmikeh/ga-wdi-extras/archive/0.1.3.tar.gz"
+  version "0.1.3"
+  sha256 ""
   head 'https://github.com/drmikeh/ga-git-extras.git', branch: 'master'
 
   # Don't take +x off these files
